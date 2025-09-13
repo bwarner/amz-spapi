2c87ccb074de31378391ce75c6c714971e85bc9c
Byron Warner <bfwarner@gmail.com>
2025-09-12 18:32:28 -0700

    feat: plain landing page

---
+++ apps/web/tailwind.config.mjs
@@ -0,0 +1,21 @@
+import { createGlobPatternsForDependencies } from '@nx/react/tailwind';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
